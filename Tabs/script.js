b4ef4a2bbcc050195d69a267a33b8e60204d1e3b
Sergey_Fadin <sergey.fadin@outlook.com>
2022-11-05 00:52:56 +0300

    work with class

--- Tabs/script.js
+++ Tabs/script.js
@@ -1,1 +1,33 @@
 'use strict';
+// const modalWindow = document.querySelector('.modal-window');
+// const overlay = document.querySelector('.overlay');
+// const btnsCloseModalWindow = document.querySelector('.close-modal-window');
+// const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
+// console.log(btnsCloseModalWindow);
+
+// for (let i = 0; i < btnsShowModalWindow.length; i++) {
+//   console.log(btnsShowModalWindow[i].textContent);
+// }
+//------------------------------Work with Class-----------------------
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
+
+const showModalWindow = function () {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModalWindow = function () {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+  btnsShowModalWindow[i].addEventListener('click', showModalWindow);
+}
+
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+
+overlay.addEventListener('click', closeModalWindow);
