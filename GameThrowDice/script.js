c65b005919ef0054b3b2700de8dd2aede41cc045
Sergey_Fadin <sergey.fadin@outlook.com>
2022-11-05 11:47:37 +0300

    выбор элементов и установка значений

--- GameThrowDice/script.js
+++ GameThrowDice/script.js
@@ -1,2 +1,8 @@
 'use strict';
-console.log('hi');
+const scoreZeroElement = document.querySelector('#score--0');
+const scoreOneElement = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+scoreZeroElement.textContent = 0;
+scoreOneElement.textContent = 0;
+diceElement.classList.add('hidden');
