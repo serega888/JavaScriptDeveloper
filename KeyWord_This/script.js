ed558a674f99b53ab60d7d53410c5083418b9f33
Sergey_Fadin <sergey.fadin@outlook.com>
2022-11-08 22:36:40 +0300

    keyword_this

---
+++ KeyWord_This/script.js
@@ -0,0 +1,38 @@
+'use strict';
+// ---------------------------------------------------------------------------------this keyword-------------------------------------------------------------
+
+// console.log(this);
+
+// const getAge = function (MybirthYear) {
+//   console.log(2021 - MybirthYear);
+//   console.log(this);
+// };
+
+// getAge(2001);
+
+// const getAgeArr = MybirthYear => {
+//   console.log(2021 - MybirthYear);
+//   console.log(this);
+// };
+
+// getAgeArr(2001);
+
+// const user187 = {
+//   MybirthYear: 1974,
+//   getAge: function () {
+//     console.log(this);
+//     console.log(2021 - this.MybirthYear);
+//   },
+// };
+
+// user187.getAge();
+
+// const user188 = {
+//   MybirthYear: 2002,
+// };
+
+// user188.getAge = user187.getAge;
+// user188.getAge();
+
+// const f = user187.getAge;
+// f();
