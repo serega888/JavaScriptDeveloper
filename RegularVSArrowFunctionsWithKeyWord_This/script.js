bb875f1b74ed86d2857090d399cf7ca5ef01a18a
Sergey_Fadin <sergey.fadin@outlook.com>
2022-11-09 23:25:18 +0300

    добавил к функциям RegularVSArrowFunction

--- RegularVSArrowFunctionsWithKeyWord_This/script.js
+++ RegularVSArrowFunctionsWithKeyWord_This/script.js
@@ -1,53 +1,53 @@
 'use strict';
 // -----------------------------------------------------------------Regular VS Arrow Functions With Keyword This-----------------------------------------------
 
-// var firstName = 'Jack';
+var firstName = 'Jack';
 
-// const user187 = {
-//   firstName: 'YouRa',
-//   MybirthYear: 1974,
-//   getAge: function () {
-//     console.log(this);
-//     console.log(2021 - this.MybirthYear);
+const userRegularVSArrowFunction387 = {
+  firstName: 'Sergey',
+  MybirthYear: 1992,
+  getAge: function () {
+    console.log(this);
+    console.log(2022 - this.MybirthYear);
 
-//     // Old approach before ES6
-//     // const self = this; // self or that
-//     // const isAdult = function () {
-//     //   console.log(self);
-//     //   console.log(2021 - self.MybirthYear >= 18);
-//     //   // console.log(2021 - this.MybirthYear >= 18);
-//     // };
+    // Old approach before ES6
+    // const self = this; // self or that
+    // const isAdultRegularVSArrowFunction = function () {
+    //   console.log(self);
+    //   console.log(2022 - self.MybirthYear >= 18);
+    //   // console.log(2022 - this.MybirthYear >= 18);
+    // };
 
-//     // New approach after ES6
-//     const isAdult = () => {
-//       console.log(this);
-//       console.log(2021 - this.MybirthYear >= 18);
-//     };
+    // New approach after ES6
+    const isAdultRegularVSArrowFunction = () => {
+      console.log(this);
+      console.log(2022 - this.MybirthYear >= 18);
+    };
 
-//     isAdult();
-//   },
-//   sayGreeting: function () {
-//     // console.log(this);
-//     console.log(`Hello, I'm ${this.firstName}`);
-//   },
-// };
+    isAdultRegularVSArrowFunction();
+  },
+  sayGreetingRegularVSArrowFunction: function () {
+    // console.log(this);
+    console.log(`Hello, I'm ${this.firstName}`);
+  },
+};
 
-// user187.sayGreeting();
-// user187.getAge();
-// // console.log(this.firstName);
+userRegularVSArrowFunction387.sayGreetingRegularVSArrowFunction();
+userRegularVSArrowFunction387.getAge();
+// console.log(this.firstName);
 
-// // arguments keyword
-// const exprSum = function (a, b) {
-//   console.log(arguments);
-//   return a + b;
-// };
+// arguments keyword
+const exprSumRegularVSArrowFunction = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
 
-// exprSum(1, 2);
-// exprSum(1, 2, 3, 9, 34);
+exprSumRegularVSArrowFunction(10, 20);
+exprSumRegularVSArrowFunction(10, 20, 30, 90, 340);
 
-// var arrSum = (a, b) => {
-//   console.log(arguments);
-//   return a + b;
-// };
+var arrSumRegularVSArrowFunction = (a, b) => {
+  //console.log(arguments);
+  return a + b;
+};
 
-// arrSum(7, 11, 23);
+arrSumRegularVSArrowFunction(70, 110, 230);
